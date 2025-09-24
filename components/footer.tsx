740691aac7df17efd235cc200e6dd859a3e34cf4
Ahmad Rafi Wirana <ahmad.rafi02@ui.ac.id>
2025-09-24 10:28:22 +0700

    update footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -4,7 +4,7 @@ import { cn } from "@/lib/utils";
 
 export function Footer() {
   return (
-    <footer role="contentinfo" className="py-2 md:py-4">
+    <footer role="contentinfo" className="py-1 md:py-2 mt-4 md:mt-6">
       <div className={cn("max-w-5xl mx-auto py-4 px-4 md:px-6")}>
         <div className="text-center text-foreground/60">
           <p className="text-sm md:text-base">
@@ -17,6 +17,18 @@ export function Footer() {
               aria-label="Visit Rafi's Twitter profile"
             >
               arayyye
+            </a>
+          </p>
+          <p className="text-sm md:text-base mt-1">
+            The code is available on{" "}
+            <a
+              href="https://github.com/ahmadrafidev/onelink"
+              className="text-foreground/90 font-medium hover:text-foreground hover:underline transition-colors duration-200 ease-out focus:outline-none focus:ring-2 focus:ring-foreground/20 focus:ring-offset-2"
+              target="_blank"
+              rel="noopener noreferrer"
+              aria-label="Visit the GitHub repository"
+            >
+              GitHub
             </a>
           </p>
         </div>
